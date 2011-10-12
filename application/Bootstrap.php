f2ffa946f5efc1d11708b39bdccea3935a03573b
Francisco S Velazquez <kikin81@gmail.com>
2011-10-11 22:33:18 -0700

    working on a better navigation

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,6 +5,23 @@
  */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    
+    //set top nav and left nav
+    protected function _initNavigation() {
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+
+        //set top navigation
+        $config = new Zend_Config_Xml(APPLICATION_PATH . "/configs/navigation.xml", "TopNavigation");
+        $navigation = new Zend_Navigation($config);
+
+        //pass to view
+        $view->topNav = $navigation;
+
+        //add to zend registry
+        Zend_Registry::set('Zend_Navigation', $leftNavigation);
+    }
 
 
 }
