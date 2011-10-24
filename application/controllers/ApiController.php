198f1a48ca379c62456d6a8ff64196d5baa44249
Victor Lara <lara.m.victor@gmail.com>
2011-10-24 16:26:38 -0700

    fixed white line issue lines and seesion api works

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -1,23 +1,20 @@
 <?php
 
-class ApiController extends Zend_Controller_Action
-{
+class ApiController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
-      $contextSwitch = $this->_helper->getHelper('contextSwitch');
-        $contextSwitch->addActionContext("getalllines", array('xml'))->initContext();
+//      $contextSwitch = $this->_helper->getHelper('contextSwitch');
+//        $contextSwitch->addActionContext("getalllines", array('xml'))->initContext();
 //        $contextSwitch->addActionContext("getalllines", array('xml'))
 //                      ->initContext();
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
-    
-    public function getallsessionsAction(){
+
+    public function getallsessionsAction() {
         $this->_helper->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender();
         //$this->_response->clearHeaders();
@@ -25,10 +22,10 @@ class ApiController extends Zend_Controller_Action
         $dom = new DOMDocument("1.0");
         $node = $dom->createElement("Sessions");
         $parnode = $dom->appendChild($node);
-       
+
         $sessionMapper = new Model_SessionMapper();
         $sessions = $sessionMapper->fetchAll();
-        foreach($sessions as $session){
+        foreach ($sessions as $session) {
             $node = $dom->createElement("Session");
             $newnode = $parnode->appendChild($node);
             $newnode->setAttribute("id", $session->getId());
@@ -37,25 +34,26 @@ class ApiController extends Zend_Controller_Action
         }
         echo $dom->saveXML();
     }
+
     //Location and Session Work
 
-    public function getalllinesAction(){
+    public function getalllinesAction() {
         $this->_helper->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender();
-        //$this->_response->clearHeaders();
         $this->_response->setHeader('Content-Type', 'text/xml; charset=utf-8');
         $dom = new DOMDocument("1.0");
         $node = $dom->createElement("Lines");
         $parnode = $dom->appendChild($node);
-       //$linesMapper = new Model_LineMapper();
-        $test = new Model_LineMapper();//########## THIS CAUSES A WHITE LINE ON TOP DONT KNOW WHY KILLS XML.......
-//        foreach($lines as $line){
-//            $node = $dom->createElement("Line");
-//            $newnode = $parnode->appendChild($node);
-//            $newnode->setAttribute("id", $line->getId());
-//            $newnode->setAttribute("name", $line->getName());
-//        }
-        echo $dom->saveXML();
+        $lineMapper = new Model_LineMapper();
+        $lines = $lineMapper->fetchAll();
+        foreach ($lines as $line) {
+            $node = $dom->createElement("Line");
+            $newnode = $parnode->appendChild($node);
+            $newnode->setAttribute("id", $line->getId());
+            $newnode->setAttribute("name", $line->getName());
+        }
+        $this->_response->setBody($dom->saveXML());
     }
+
 }
 
