88d0cb43138275728955539ab3042356f6c6fdd3
Victor Lara <lara.m.victor@gmail.com>
2011-10-31 14:36:11 -0700

    cleaned up adminController code

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -41,7 +41,6 @@ class AdminController extends Zend_Controller_Action {
         //DaysOfOperation
         $doMapper = new Model_DaysOperationMapper();
         $this->view->dos = $doMapper->fetchAll();
-        // die(var_dump($this->views->dos ));
     }
 
     /**
@@ -103,8 +102,6 @@ class AdminController extends Zend_Controller_Action {
                             $finalTime = $t + 1200;
                         }
                     }
-
-
                     $arrival = new Model_Arrival();
                     $arrival->setLine($this->_getParam("line"));
                     $arrival->setLocation($locationID);
@@ -121,11 +118,8 @@ class AdminController extends Zend_Controller_Action {
     public function managearrivalAction() {
         $arrivalMapper = new Model_ArrivalMapper();
         $request = $this->getRequest();
-        print_r($request);
-        //$form = new Form_Arrival();
 
         if ($this->getRequest()->isPost()) {
-            //if($form->isValid($request->getPost())){
             $arrival = new Model_Arrival();
             if (strlen($this->_getParam('ArrivalID')) > 0) {
                 $id = $this->_getParam('ArrivalID');
@@ -138,7 +132,6 @@ class AdminController extends Zend_Controller_Action {
             $arrivalMapper->save($arrival);
             createLocationsXml();
             return $this->_helper->redirector('unifiedadmin');
-            //}
         }
     }
 
@@ -159,13 +152,9 @@ class AdminController extends Zend_Controller_Action {
     }
 
     public function managelinesAction() {
-        // Query all 
-        //$this->view->headScript()->appendFile("/js/deleteLine.js");
-        //$this->view->headScript()->appendFile("/js/managelines.js");
         $lineMapper = new Model_LineMapper();
         $dooMapper = new Model_DaysOperationMapper();
         $this->view->currentLines = $lineMapper->fetchAll();
-
         $request = $this->getRequest();
         $form = new Form_Line();
 
@@ -192,7 +181,6 @@ class AdminController extends Zend_Controller_Action {
                 $doo = new Model_DaysOperation();
 
                 $doo->setLineID($lineID);
-                // followed by a for loop for the days
                 if ($this->_getParam('M')) {
                     $doo->setDay(1);
                     $dooMapper->save($doo);
@@ -221,70 +209,26 @@ class AdminController extends Zend_Controller_Action {
                     $doo->setDay(7);
                     $dooMapper->save($doo);
                 }
-                //finally redirect
                 return $this->_helper->redirector('unifiedadmin');
             }
         }
         $this->view->form = $form;
     }
 
-    /* CRR Action to create, remove, rename a line */
-
     public function crrAction() {
         $this->_helper->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender();
-
-        // crrm = create, remove, rename, move operations
         $operation = $this->_getParam("operation");
         $lineMapper = new Model_LineMapper();
         $locationMapper = new Model_LocationMapper();
         $arrivalMapper = new Model_ArrivalMapper();
         $sessionMapper = new Model_SessionMapper();
         switch ($operation) {
-            case "create":
-                break;
             case "remove":
                 $id = $this->_getParam("lineID");
                 $lineMapper->deleteLine($id);
                 $myFile = getcwd() . "/xml/Line-" . $id . ".xml";
                 unlink($myFile);
-                break;
-            case "rename":
-                $data = array(
-                    'id' => $this->_getParam('id'),
-                    'name' => $this->_getParam('value')
-                );
-                $line = new Model_Line($data);
-                $lineMapper->save($line);
-                echo $this->_getParam('value');
-                break;
-            case "renameName":
-                $location = new Model_Location();
-                $locationMapper->find($this->_getParam('id'), $location);
-                $location->setName($this->_getParam('value'));
-                $locationMapper->save($location);
-                echo $this->_getParam('value');
-                break;
-            case "renameLongitude":
-                $location = new Model_Location();
-                $locationMapper->find($this->_getParam('id'), $location);
-                $location->setLng($this->_getParam('value'));
-                $locationMapper->save($location);
-                echo $this->_getParam('value');
-                break;
-            case "renameLatitude":
-                $location = new Model_Location();
-                $locationMapper->find($this->_getParam('id'), $location);
-                $location->setLat($this->_getParam('value'));
-                $locationMapper->save($location);
-                echo $this->_getParam('value');
-                break;
-            case "renameDescription":
-                $location = new Model_Location();
-                $locationMapper->find($this->_getParam('id'), $location);
-                $location->setDescription($this->_getParam('value'));
-                $locationMapper->save($location);
-                echo $this->_getParam('value');
                 break;
             case "removeLocation":
                 $locationMapper->delete($this->_getParam('locationID'));
@@ -292,29 +236,8 @@ class AdminController extends Zend_Controller_Action {
             case "removeArrival":
                 $arrivalMapper->delete($this->_getParam('arrivalID'));
                 break;
-            case "arrivalRenameLine":
-                $arrival = new Model_Arrival();
-                $arrivalMapper->find($this->_getParam('sessionID'), $arrival);
-                print_r($arrival);
-                $arrival->setLine($this->_getParam('value'));
-                $arrivalMapper->save($arrival);
-                break;
             case "removeSession":
                 $sessionMapper->delete($this->_getParam('sessionID'));
-                break;
-            case "editDescriptionSession":
-                $session = new Model_Session();
-                $sessionMapper->find($this->_getParam('id'), $session);
-                $session->setDesc($this->_getParam('value'));
-                $sessionMapper->save($session);
-                echo $this->_getParam('value');
-                break;
-            case "editActiveSession":
-                $session = new Model_Session();
-                $sessionMapper->find($this->_getParam('id'), $session);
-                $session->setActive($this->_getParam('value'));
-                $sessionMapper->save($session);
-                echo $this->_getParam('value');
                 break;
         }
     }
@@ -351,6 +274,7 @@ class AdminController extends Zend_Controller_Action {
         $locationMapper = new Model_LocationMapper();
         $lineMapper = new Model_LineMapper();
         $arrivals = $arrivalMapper->fetchAll($id);
+        
         foreach ($arrivals as $arrival) {
             $location = new Model_Location();
             $locationMapper->find($arrival->getLocation(), $location);
@@ -383,7 +307,6 @@ class AdminController extends Zend_Controller_Action {
         $fh = fopen($myFile, 'w') or die("can't open file");
         fwrite($fh,  $dom->saveXML());
         fclose($fh);
-        //$dom->saveXML();
     }
     
         public function createLineXml($line){
